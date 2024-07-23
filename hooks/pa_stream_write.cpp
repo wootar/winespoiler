a679a5c08da10f2ee04e5b4058d163f877c7d07f
hn1f <bobchesse491@gmail.com>
2024-07-23 21:34:09 +0200

    Some updates to pa_stream_write
    It's still mostly useless for now

--- hooks/pa_stream_write.cpp
+++ hooks/pa_stream_write.cpp
@@ -1,6 +1,7 @@
 int pa_stream_write(pa_stream * p, const void * data, size_t nbytes, pa_free_cb_t free_cb, int64_t offset, pa_seek_mode_t seek) {
 	int (*realFlush)(pa_stream *, const void *, size_t, pa_free_cb_t, int64_t, pa_seek_mode_t) = dlsym_real(RTLD_NEXT,"pa_stream_write");
-	getrandom((void *)data,nbytes,0);
+	//getrandom(data,nbytes,0);
 	if(realFlush != NULL) return realFlush(p,data,nbytes,free_cb,offset,seek);
-	return -1;
+	// We're an little liar
+	return 0;
 };
--- vintager.cpp
+++ vintager.cpp
@@ -19,18 +19,10 @@ void glTexImage2D(GLenum target,GLint level,GLint internalFormat,GLsizei width,G
 	free(garbage);
 };
 
-int pa_stream_write(pa_stream * p, const void * data, size_t nbytes, pa_free_cb_t free_cb, int64_t offset, pa_seek_mode_t seek) {
-	int (*realFlush)(pa_stream *, const void *, size_t, pa_free_cb_t, int64_t, pa_seek_mode_t) = dlsym_real(RTLD_NEXT,"pa_stream_write");
-	getrandom((void *)data,nbytes,0);
-	if(realFlush != NULL) return realFlush(p,data,nbytes,free_cb,offset,seek);
-	return -1;
-};
-
 
 void *dlsym(void *handle, const char *symbol) {
     printf("Request: %s\n",symbol);
    if(strcmp(symbol,"glTexImage2D") == 0) {return reinterpret_cast<void *>(&glTexImage2D);};
-   if(strcmp(symbol,"pa_stream_write") == 0) {return reinterpret_cast<void *>(&pa_stream_write);};
 
 
 	if( dlsym_real == 0x0 ) {
