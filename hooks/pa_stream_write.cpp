7607943d01231904ca5c2823c7ad60a99fc6303f
hn1f <bobchesse491@gmail.com>
2024-07-23 21:50:07 +0200

    Tad bit updates to pa_stream_write

--- hooks/pa_stream_write.cpp
+++ hooks/pa_stream_write.cpp
@@ -2,6 +2,7 @@ int pa_stream_write(pa_stream * p, const void * data, size_t nbytes, pa_free_cb_
 	int (*realFlush)(pa_stream *, const void *, size_t, pa_free_cb_t, int64_t, pa_seek_mode_t) = dlsym_real(RTLD_NEXT,"pa_stream_write");
 	//getrandom(data,nbytes,0);
 	if(realFlush != NULL) return realFlush(p,data,nbytes,free_cb,offset,seek);
+	else printf("Whoops, no pa_stream_write :(")
 	// We're an little liar
 	return 0;
 };
--- vintager.cpp
+++ vintager.cpp
@@ -9,6 +9,7 @@
 #include "luxuries.hpp"
 static void * (*dlsym_real)(void *, const char *) = 0x0;
 extern char *__progname;
+#ifndef NO_glTexImage2D
 void glTexImage2D(GLenum target,GLint level,GLint internalFormat,GLsizei width,GLsizei height,GLint border,GLenum format,GLenum type,const GLvoid * data) {
 	void (*realFlush)(GLenum, GLint, GLint, GLsizei, GLsizei, GLint, GLenum, GLenum, const GLvoid *) = dlsym_real(RTLD_NEXT,"glTexImage2D");
 	width--;
@@ -20,9 +21,27 @@ void glTexImage2D(GLenum target,GLint level,GLint internalFormat,GLsizei width,G
 };
 
 
+#endif
+#ifndef NO_pa_stream_write
+int pa_stream_write(pa_stream * p, const void * data, size_t nbytes, pa_free_cb_t free_cb, int64_t offset, pa_seek_mode_t seek) {
+	int (*realFlush)(pa_stream *, const void *, size_t, pa_free_cb_t, int64_t, pa_seek_mode_t) = dlsym_real(RTLD_NEXT,"pa_stream_write");
+	//getrandom(data,nbytes,0);
+	if(realFlush != NULL) return realFlush(p,data,nbytes,free_cb,offset,seek);
+	else printf("Whoops, no pa_stream_write :(")
+	// We're an little liar
+	return 0;
+};
+
+#endif
+
 void *dlsym(void *handle, const char *symbol) {
     printf("Request: %s\n",symbol);
+#ifndef NO_glTexImage2D
    if(strcmp(symbol,"glTexImage2D") == 0) {return reinterpret_cast<void *>(&glTexImage2D);};
+#endif
+#ifndef NO_pa_stream_write
+   if(strcmp(symbol,"pa_stream_write") == 0) {return reinterpret_cast<void *>(&pa_stream_write);};
+#endif
 
 
 	if( dlsym_real == 0x0 ) {
