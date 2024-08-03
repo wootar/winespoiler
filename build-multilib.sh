5bfe0a46e3ce18d3567a95f6cfcea6af444fafc6
hn1f <bobchesse491@gmail.com>
2024-08-03 08:51:23 +0200

    Fix the multilib build script

--- build-multilib.sh
+++ build-multilib.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
-echo "Compiling 32-bit winespoiler"
-CFLAGS="-m32" ./build.sh
-mv winespoiler.so winespoiler32.so
-echo "Compiling 64-bit winespoiler"
-CFLAGS="-m64" ./build.sh
+echo "Compiling 32-bit version of winespoiler and the 64-bit version of winespoiler"
+multilib=1 ./build.sh
